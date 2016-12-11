57b130a2ccaefe0d9be01923758cbf37908b4ad9
Harvey Shi <harveyyshi@gmail.com>
2016-12-10 20:38:30 -0500

    updated spark library to particle

--- led-control.js
+++ led-control.js
@@ -1,63 +1,49 @@
 //require spark
-var spark = require('spark');
-var key = require('./led-key.js');
-var token = key.getKey();
+var Particle = require('particle-api-js'),
+    particle = new Particle();
 
-// login to spark/particle
-spark.login({
-    accessToken: token
-});
+var key = require('./led-key.js');
+var token = key.getToken(),
+    ID = key.getID();
+
+
+function callFxn(name, argument) {
+    particle.callFunction({
+        deviceId: ID,
+        name: name,
+        argument: argument,
+        auth: token
+    });
+}
 
 module.exports = {
     ledUpdate: function (type, rgbArray) {
         console.log('LED control: ' + type);
-	switch (type) {
-        case 'off':
-            spark.listDevices(function (err, devices) {
-                devices[0].callFunction('setRGB', 'off');
-            });
-            break;
-        case 'warm':
-            spark.listDevices(function (err, devices) {
-                devices[0].callFunction('setRGB', '154,84,17');
-            });
-
-            break;
-        case 'on':
-            spark.listDevices(function (err, devices) {
-                devices[0].callFunction('setRGB', '100,100,100');
-            });
-
-            break;
-        case 'high':
-            spark.listDevices(function (err, devices) {
-                devices[0].callFunction('setRGB', '255,255,255');
-            });
-            break;
-
-            break;
-        case 'red':
-            spark.listDevices(function (err, devices) {
-                devices[0].callFunction('setRGB', '200,0,0');
-            });
-
-            break;
-        case 'green':
-            spark.listDevices(function (err, devices) {
-                devices[0].callFunction('setRGB', '0,200,0');
-            });
-
-            break;
-        case 'blue':
-            spark.listDevices(function (err, devices) {
-                devices[0].callFunction('setRGB', '0,0,200');
-            });
-            break;
-        case 'party':
-            spark.listDevices(function (err, devices) {
-                devices[0].callFunction('lightParty', 'yolo');
-            });
-            break;
+        switch (type) {
+            case 'off':
+                callFxn('setRGB', 'off');
+                break;
+            case 'warm':
+                callFxn('setRGB', '154,84,17');
+                break;
+            case 'on':
+                callFxn('setRGB', '100,100,100');
+                break;
+            case 'high':
+                callFxn('setRGB', '255,255,255');
+                break;
+            case 'red':
+                callFxn('setRGB', '200,0,0');
+                break;
+            case 'green':
+                callFxn('setRGB', '0,200,0');
+                break;
+            case 'blue':
+                callFxn('setRGB', '0,0,200');
+                break;
+            case 'party':
+                callFxn('lightParty', 'yolo');
+                break;
         }
     }
 };
