99387820c9eab472968bf4655ea5f59ea5c9c503
Harvey Shi <harveyyshi@gmail.com>
2018-11-04 22:22:10 -0500

    New names for lights and adds people's names.

--- public/js/main.js
+++ public/js/main.js
@@ -1,73 +1,104 @@
-$(function () {
-    FastClick.attach(document.body);
-    var lights = [["Dirty Lab", "26", "labs"], ["Clean Lab Cabinets", "3", "labs"], ["Clean Lab", "4", "labs"], ["South West Bedroom", "6", "living"], ["Downstairs Bedroom", "7", "living"], ["North West Bedroom", "9", "living"], ["North East Bedroom", "21", "living"], ["South East Bedroom", "28", "living"], ["West Balcony", "35", "outside"], ["Front Porch", "35", "outside"], ["Back Porch", "36", "outside"], ["Kitchen", "11", "community"], ["Front Indoor Lights", "12", "community"], ["White Board Lights", "31", "community"], ["Kitchen Cabinets", "38", "community"], ["Main Room", "11,12,31", "community"], ["Media Room", "20,24", "community"], ["Upper Floor", "0,2", "community"], ["East Upper Bathroom", "17", "bathrooms"], ["West Upper Bathroom", "15", "bathrooms"], ["West Lower Bathroom", "13", "bathrooms"]];
-    lights.sort(function (a, b) {
-        if (a[0] === b[0]) {
-            return 0;
-        } else {
-            return (a[0] > b[0]) ? -1 : 1;
-        }
-    });
-
-    function addLights(lightArray) {
-        for (var i = 0; i < lights.length; ++i) {
-            var html = '<div class="lightGroup" data-light="' + lights[i][1] + '" data-type="' + lights[i][2] + '"><div class="name">' + lights[i][0] + ': <span>OFF</span></div><div class="slider"><div class="slider-handle"></div></div></div>'
-            $('#groupContainer').prepend(html);
-        }
+$(function() {
+  FastClick.attach(document.body);
+  var lights = [
+    ['Dirty Lab', '26', 'labs'],
+    ['Clean Lab Cabinets', '3', 'labs'],
+    ['Clean Lab', '4', 'labs'],
+    ['Anuj & Matt', '6', 'living'],          // SW
+    ['David & Ricardo', '7', 'living'],      // Downstairs
+    ['Flo & Lauren', '9', 'living'],         // NW
+    ['Elizabeth & Sakura', '21', 'living'],  // NE
+    ['Mac & Rebecca', '28', 'living'],       // SE
+    ['West Balcony', '35', 'outside'],
+    ['Front Porch', '35', 'outside'],
+    ['Back Porch', '36', 'outside'],
+    ['Stairs', '12', 'community'],
+    ['White Boards', '31', 'community'],
+    ['Kitchen', '11', 'community'],
+    ['Kitchen Cabinets', '38', 'community'],
+    ['Main Room', '11,12,31', 'community'],
+    ['Media Room', '20,24', 'community'],
+    ['Upper Floor', '0,2', 'community'],
+    ['East Upper Bathroom', '17', 'bathrooms'],
+    ['West Upper Bathroom', '15', 'bathrooms'],
+    ['West Lower Bathroom', '13', 'bathrooms']
+  ];
+  lights.sort(function(a, b) {
+    if (a[0] === b[0]) {
+      return 0;
+    } else {
+      return (a[0] > b[0]) ? -1 : 1;
     }
-    addLights(lights);
+  });
 
-    // organize categories
-    var categories = [["All", "all"], ["Social", "community"], ["Rooms", "living"], ["Bath", "bathrooms"], ["Labs", "labs"], ["Outside", "outside"]];
-
-    for (var i = 0; i < categories.length; ++i) {
-        var html = '<div class="sortButton" id="' + categories[i][1] + '"><div class="title">' + categories[i][0] + '</div><div class="logow" style="background-image: url(./img/' + categories[i][0].toLowerCase() + '_w.svg)"></div></div>'
-        $('#sortContainer>div').append(html);
+  function addLights(lightArray) {
+    for (var i = 0; i < lights.length; ++i) {
+      var html = '<div class="lightGroup" data-light="' + lights[i][1] +
+          '" data-type="' + lights[i][2] + '"><div class="name">' +
+          lights[i][0] +
+          ': <span>OFF</span></div><div class="slider"><div class="slider-handle"></div></div></div>'
+      $('#groupContainer').prepend(html);
     }
-    // select all button
-    $('#all').addClass('selected');
+  }
+  addLights(lights);
 
-    // click behavior
-    $(".sortButton").on('click', function () {
-        // deal with previous
-        $('.selected').removeClass('selected');
+  // organize categories
+  var categories = [
+    ['All', 'all'], ['Social', 'community'], ['Rooms', 'living'],
+    ['Bath', 'bathrooms'], ['Labs', 'labs'], ['Outside', 'outside']
+  ];
 
-        // update clicker
-        $(this).addClass('selected');
+  for (var i = 0; i < categories.length; ++i) {
+    var html = '<div class="sortButton" id="' + categories[i][1] +
+        '"><div class="title">' + categories[i][0] +
+        '</div><div class="logow" style="background-image: url(./img/' +
+        categories[i][0].toLowerCase() + '_w.svg)"></div></div>'
+    $('#sortContainer>div').append(html);
+  }
+  // select all button
+  $('#all').addClass('selected');
 
-        if ($(this).attr('id') == 'all') { // show all
-            $('.lightGroup').fadeIn(0);
-        } else {
-            $('.lightGroup').fadeIn(0);
-            $('.lightGroup:not([data-type="' + $(this).attr('id') + '"])').fadeOut(0);
-        }
-    });
+  // click behavior
+  $('.sortButton').on('click', function() {
+    // deal with previous
+    $('.selected').removeClass('selected');
 
-    // initialize socket.io
-    var socket = io();
+    // update clicker
+    $(this).addClass('selected');
 
-    // handle switching lights
-    $(".lightGroup").on('click', function () {
-        var status = $(this).children('.name').children('span').html();
-        var group = $(this).attr('data-light');
-        if (status == "OFF") {
-            socket.emit('change status', 'ON.' + group);
-        } else {
-            socket.emit('change status', 'OFF.' + group);
-        }
-    });
+    if ($(this).attr('id') == 'all') {  // show all
+      $('.lightGroup').fadeIn(0);
+    } else {
+      $('.lightGroup').fadeIn(0);
+      $('.lightGroup:not([data-type="' + $(this).attr('id') + '"])').fadeOut(0);
+    }
+  });
 
-    // update status if another user changes it
-    socket.on('change status', function (twoMsg) {
-        var msgStrs = twoMsg.split(".");
-        var msg = msgStrs[0];
-        var data = msgStrs[1];
-        var $this = $("[data-light='" + data + "']");
-        $this.children('.name').children('span').html(msg);
-        if (msg == "ON") {
-            $this.addClass('on');
-        } else {
-            $this.removeClass('on');
-        }
-    });
+  // initialize socket.io
+  var socket = io();
+
+  // handle switching lights
+  $('.lightGroup').on('click', function() {
+    var status = $(this).children('.name').children('span').html();
+    var group = $(this).attr('data-light');
+    if (status == 'OFF') {
+      socket.emit('change status', 'ON.' + group);
+    } else {
+      socket.emit('change status', 'OFF.' + group);
+    }
+  });
+
+  // update status if another user changes it
+  socket.on('change status', function(twoMsg) {
+    var msgStrs = twoMsg.split('.');
+    var msg = msgStrs[0];
+    var data = msgStrs[1];
+    var $this = $('[data-light=\'' + data + '\']');
+    $this.children('.name').children('span').html(msg);
+    if (msg == 'ON') {
+      $this.addClass('on');
+    } else {
+      $this.removeClass('on');
+    }
+  });
 });
